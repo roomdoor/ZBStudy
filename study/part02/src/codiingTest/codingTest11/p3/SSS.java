e739e8bdf63b67533f1c463ccf790f0dfd097db4
roomdoor <roomdoor.lee@gmail.com>
2022-08-09 13:03:17 +0900

    8/9

--- study/part02/src/codiingTest/codingTest11/p3/SSS.java
+++ study/part02/src/codiingTest/codingTest11/p3/SSS.java
@@ -1,40 +1,25 @@
 package codiingTest.codingTest11.p3;
 
-import java.util.Arrays;
-import java.util.Objects;
+public class SSS {
+    static boolean result;
 
-// String 메서드를 이용하여 푸는 문제
-public class Solution {
     public static boolean solution(String s, String[] words) {
-        boolean answer = false;
-        if (s.length() == 0 && words.length == 0) {
-            return true;
-        }
-        // words 의 단어로 시작된다면 find 메서드 시작
-        for (String word : words) {
-            if (s.startsWith(word)) {                               // find 함수 재귀로 탐색
-                answer = find(s.substring(word.length()), words);    // words 에 단어로 시작된다면 s 에서 words 단어를 제거하고 재귀함수에 넣어줌
-            }
-            if (answer) {
-                return true;
-            }
-        }
-
-        return false;
+        result = false;
+        dfs(s, words);
+        return result;
     }
 
-    public static boolean find(String s, String[] words) {   // 재귀 함수
-        if (s.length() == 0) {                              // 남아있는 s 문자열이 없다면 true 반환
-            return true;
+    static void dfs(String s, String[] words) {
+        if (s.equals("")) {
+            result = true;
+            return;
         }
 
-        for (String word : words) {                         // 다시 words 에 단어로 시작하는지 탐색
-            if (s.startsWith(word)) {
-                String substring = s.substring(word.length());  // 시작한다면 sub String 만들어서 다시 find 함수 실행
-                return find(substring, words);
+        for (String w: words) {
+            if (s.startsWith(w)) {
+                dfs(s.substring(w.length()), words);
             }
         }
-        return false;
     }
 
     public static void main(String[] args) {
