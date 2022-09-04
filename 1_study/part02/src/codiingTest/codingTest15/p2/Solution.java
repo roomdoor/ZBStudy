ae3c5e8911192454ef69b98a577c902687857c00
roomdoor <roomdoor.lee@gmail.com>
2022-09-04 13:22:31 +0900

    9/3

--- 1_study/part02/src/codiingTest/codingTest15/p2/Solution.java
+++ 1_study/part02/src/codiingTest/codingTest15/p2/Solution.java
@@ -3,23 +3,43 @@ package codiingTest.codingTest15.p2;
 
 import java.util.Arrays;
 
-// 부릍포스로 하는 방법 말고는 생각이 나질 않습니다.
-// 구현하지 않았지만 부르트포스로 구현하게 되면 1000만이
-// 넘어가는 for 문을 돌아야 하므로 시간초가과 무조껀 발생할 것같습니다.
 public class Solution {
-    public int solution(int[] buckets, int m) {
+    public static int solution(int[] buckets, int m) {
         int answer = 0;
         int len = buckets.length;
         Arrays.sort(buckets);
-        int[] interval = new int[len - 1];
-        for (int i = 1; i < len; i++) {
-            interval[i] = buckets[i - 1] - buckets[i];
+
+        int left = 0;
+        int right = buckets[len - 1];
+
+        while (left <= right) {
+            int dis = left + (right - left) / 2;
+            int ballNum = 1;
+            int curBucket = buckets[0];
+
+            for (int i = 1; i < len; i++) {
+                if (buckets[i] - curBucket >= dis) {
+                    ballNum++;
+                    curBucket = buckets[i];
+                }
+                if (ballNum >= m) {
+                    break;
+                }
+            }
+
+            if (ballNum >= m) {
+                answer = Math.max(answer, dis);
+                left = dis + 1;
+            } else {
+                right = dis - 1;
+            }
         }
-
-
         return answer;
     }
-}
 
-// 0    0   0   0   0   0   0   0   0   0
-// 1                1                   1
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 4, 9, 99999};
+        int b = 2;
+        System.out.println(solution(a, b));
+    }
+}
--- 1_study/part02/src/codiingTest/codingTest15/p3/Solution.java
+++ 1_study/part02/src/codiingTest/codingTest15/p3/Solution.java
@@ -1,34 +1,27 @@
 package codiingTest.codingTest15.p3;
 
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 
-// 2번 문제와 마찬가지로 브릍포스 말고는 생각이 나질 않습니다.
-// 정확성은 모두 통과햐였지만 효율성에서 모두 시간초과를 받았습니다.
 public class Solution {
     public static int solution(int[] arr, int k) {
         int len = arr.length;
-        int[] dp = new int[len];                // 현재 자리에서 가질 수 있는 가장 큰 값 저장
-        Arrays.fill(dp, Integer.MIN_VALUE);      // dp 초기화
-        dp[0] = arr[0];
 
-        for (int i = 0; i < len; i++) {         // arr 모든 변수 돌기
-            for (int j = 1; j <= k; j++) {      // i 자리에서 k만큼 갈 수 있는 곳 dp에 업데이트
-                if (i + j >= len) {
-                    break;
-                }
-                dp[i + j] = Math.max(dp[i + j], dp[i] + arr[i + j]);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((x, y) -> y[1] - x[1]);
+        queue.add(new int[]{0, arr[0]});
+
+        for (int i = 1; i < len; i++) {
+            while (!queue.isEmpty() && queue.peek()[0] < i - k) {
+                queue.poll();
             }
+            queue.add(new int[]{i, queue.peek()[1] + arr[i]});
         }
 
+        while (queue.peek()[0] != len - 1) {
+            queue.poll();
+        }
 
-        return dp[len - 1];                     // dp 에 기록된 마지막 값 리턴
-    }
-
-    public static void main(String[] args) {
-        int[] a = new int[]{3, -4, 5, 1, 3, -5, -12, 4, -4, 5};
-        System.out.println(solution(a, 3));
-
+        return queue.peek()[1];
     }
 }
